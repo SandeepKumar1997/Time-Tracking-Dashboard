bff1c88a5a464ef05731a3018a061da5df48df0e
- <sandeepkumarhari@dxc.com>
2022-01-09 20:35:00 +0530

    position's of card div's are added

---
+++ app.js
@@ -0,0 +1,19 @@
+let btnDaily=document.querySelector('.nav-btn-daily');
+let btnWeekly=document.querySelector('.nav-btn-weekly');
+let btnMonthly=document.querySelector('.nav-btn-monthly');
+
+btnDaily.addEventListener("click",()=>{
+ console.log("Daily is clicked");
+});
+
+
+btnWeekly.addEventListener("click",()=>{
+ console.log("weekly is clicked");
+});
+
+
+btnMonthly.addEventListener("click",()=>{
+ console.log("monthly is clicked");
+});
+
+
